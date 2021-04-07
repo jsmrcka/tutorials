72a0a913f45b6b157229ef05a05a5dfb4d248d05
Josef Smrcka <jsmrcka@redhat.com>
2021-04-07 14:12:42 +0200

    Home page test

---
+++ spring-boot-modules/spring-boot-bootstrap/src/test/java/com/baeldung/HomePageTest.java
@@ -0,0 +1,23 @@
+package com.baeldung;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+public class HomePageTest {
+    private static final String APP_NAME = "Bootstrap Spring Boot";
+
+    @Test
+    public void testHomePage() {
+        RestAssured.get("/")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.HTML)
+                .body(CoreMatchers.containsString(APP_NAME));
+    }
+}
