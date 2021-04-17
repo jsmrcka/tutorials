5dca0826b3d064e801ad2b8479ee7b159a1e0db3
Josef Smrcka <jsmrcka@redhat.com>
2021-04-17 18:24:30 +0200

    Delete unused code
    Get rid of XML config (unsupported) and related classes.

--- spring-di/src/main/java/com/baeldung/di/spring/SpringMainConfig.java
+++ spring-di/src/main/java/com/baeldung/di/spring/SpringMainConfig.java
@@ -1,13 +1,9 @@
 package com.baeldung.di.spring;
 
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
 
 @Configuration
-//@Import({ SpringBeansConfig.class })
-//@ComponentScan("com.baeldung.di.spring")
 public class SpringMainConfig {
 
 	@Bean
